dcea3951fd24cb9307f3404d17d0beefdea23aeb
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-10 18:18:02 +0530

    Update LoginPage.ts

--- Day 3/pages/LoginPage.ts
+++ Day 3/pages/LoginPage.ts
@@ -1,4 +1,5 @@
 import { Page, Locator, expect } from '@playwright/test'; 
+import { routes } from '../Constants/routes';
  
 export class LoginPage { 
   readonly page: Page; 
@@ -16,7 +17,7 @@ export class LoginPage {
   } 
  
   async goto(): Promise<void> { 
-    await this.page.goto('https://www.saucedemo.com/'); 
+    await this.page.goto(routes.Login); 
   } 
  
   async login(username: string, password: string): Promise<void> { 
