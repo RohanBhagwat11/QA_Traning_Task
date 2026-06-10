740f4cb0915c189bccbf0581f0de6051c8225aa0
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-10 18:20:42 +0530

    Add files via upload

---
+++ Day 3/test-data/userdata.ts
@@ -0,0 +1,19 @@
+export interface Userdata { 
+  firstname: string; 
+  lastname: string; 
+  postalcode: string;
+} 
+ 
+export const userdata: Userdata[] = [ 
+  { 
+    firstname: 'Rohan',
+    lastname: 'Bhagwat',
+    postalcode: '400620'
+  }, 
+    { 
+    firstname: 'Rahul',
+    lastname: 'Gupta',
+    postalcode: '405400'
+  }, 
+  
+]; 
