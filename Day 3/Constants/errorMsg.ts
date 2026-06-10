626372775fe661c01f3c88c7e87f5b34ca383224
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-10 18:21:33 +0530

    Add files via upload

---
+++ Day 3/Constants/errorMsg.ts
@@ -0,0 +1,8 @@
+export const errorMsg = {
+    firstNameRequired : 'First Name is required',
+    lastNameRequired  : 'Last Name is required',
+    postalCodeRequired : 'Postal Code is required',
+    NoUserPassMatch : 'Username and password do not match',
+    lockedUserMsg : 'Sorry, this user has been locked out',
+    OrderSuccess: 'Thank you for your order'
+}
---
+++ Day 3/Constants/routes.ts
@@ -0,0 +1,13 @@
+
+export const routes = {
+    Login : "https://www.saucedemo.com/",
+    Facebook: "https://www.facebook.com/saucelabs",
+    Twitter: "https://x.com/saucelabs",
+    Linkedin: "https://www.linkedin.com/company/sauce-labs/",
+    checkout: "https://www.saucedemo.com/checkout-step-one.html",
+    Product: "https://www.saucedemo.com/inventory.html",
+    checkoutComplete: "https://www.saucedemo.com/checkout-complete.html",
+    cart: "https://www.saucedemo.com/cart.html"
+
+}
+
