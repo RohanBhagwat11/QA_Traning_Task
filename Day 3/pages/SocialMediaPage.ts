8c3c640017544ff22876dabaee4709c1cb14419c
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-15 11:42:40 +0530

    Update SocialMediaPage.ts

--- Day 3/pages/SocialMediaPage.ts
+++ Day 3/pages/SocialMediaPage.ts
@@ -1,42 +1,42 @@
-import { Page, Locator, expect } from '@playwright/test'; 
-import {routes} from '../Constants/routes'
+import { Page, Locator, expect } from "@playwright/test";
+import { routes } from "../Constants/routes";
 
-export class SocialMediaPage{
-    readonly page: Page
+export class SocialMediaPage {
+  readonly page: Page;
 
-    constructor(page:Page){
-        this.page = page
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    }
+  async gotoTwitter(): Promise<void> {
+    await this.page.goto(routes.Twitter);
+  }
 
-    async gotoTwitter():Promise<void>{
-        await this.page.goto(routes.Twitter)
-    }
+  async verifyTwitterPage(): Promise<void> {
+    await expect(
+      this.page.getByRole("link", { name: "X", exact: true }),
+    ).toBeVisible();
+  }
 
-    async verifyTwitterPage(): Promise<void>{
-    
-        await expect(this.page.getByRole('link', { name: 'X', exact: true })).toBeVisible();
-       }
+  async gotoFacebook(): Promise<void> {
+    await this.page.goto(routes.Facebook);
+  }
 
-     async gotoFacebook():Promise<void>{
-        await this.page.goto(routes.Facebook)
-    }
+  async verifyFacebookPage(): Promise<void> {
+    await this.page.getByRole("button", { name: "Close" }).click();
+    await expect(
+      this.page.getByRole("button", { name: "Facebook" }),
+    ).toBeVisible();
+  }
 
-    async verifyFacebookPage(): Promise<void>{
-    
-    await this.page.getByRole('button', { name: 'Close' }).click();
-    await expect(this.page.getByRole('button', { name: 'Facebook' })).toBeVisible();
-   
-    }
-    
+  async gotoLinkedin(): Promise<void> {
+    await this.page.goto(routes.Linkedin);
+  }
 
-     async gotoLinkedin():Promise<void>{
-        await this.page.goto(routes.Linkedin)
-    }
-
-    async verifyLinkedinPage(): Promise<void>{
-    
-   await this.page.getByRole('button', { name: 'Dismiss' }).click();
-   await expect(this.page.getByRole('link', { name: 'LinkedIn', exact: true })).toBeVisible();
-    }
+  async verifyLinkedinPage(): Promise<void> {
+    await this.page.getByRole("button", { name: "Dismiss" }).click();
+    await expect(
+      this.page.getByRole("link", { name: "LinkedIn", exact: true }),
+    ).toBeVisible();
+  }
 }
