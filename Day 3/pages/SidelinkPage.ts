e8ff40ee6940671dfe22b61a1f09f7f7174ffe54
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-15 11:44:32 +0530

    Add files via upload

---
+++ Day 3/pages/SidelinkPage.ts
@@ -0,0 +1,21 @@
+import { Page, Locator, expect } from "@playwright/test";
+
+export class SidelinkPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async logout(): Promise<void> {
+    await this.page.getByRole("button", { name: "Open Menu" }).click();
+    await this.page.locator('[data-test="logout-sidebar-link"]').click();
+  }
+
+  async verifyLogout(): Promise<void> {
+    await expect(this.page.locator('[data-test="login-button"]')).toBeVisible();
+    await expect(this.page.locator('[data-test="username"]')).toBeVisible();
+    await expect(this.page.locator('[data-test="password"]')).toBeVisible();
+    await expect(this.page.locator('[data-test="login-button"]')).toBeVisible();
+  }
+}
