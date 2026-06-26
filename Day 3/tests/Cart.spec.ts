9aca1d9be5e895575b2b67e984b1ef15dfdeaa84
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-26 12:51:56 +0530

    Update Cart.spec.ts

--- Day 3/tests/Cart.spec.ts
+++ Day 3/tests/Cart.spec.ts
@@ -4,63 +4,36 @@ import { CartPage } from "../pages/CartPage";
 import { products } from "../test-data/product";
 import { loginAsStandardUser } from "../utils/testHelpers";
 
+let productPage: ProductsPage;
+let cartPage: CartPage;
 
 test.describe("Cart Tests", () => {
-/*  test("TC_006 - Add one product to cart @regression", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
+    productPage = new ProductsPage(page);
+    cartPage = new CartPage(page);
     await loginAsStandardUser(page);
-    const productsPage = new ProductsPage(page);
-    await productsPage.addProductToCart(products[0].name);
-    await productsPage.verifyCartCount(1);
   });
-  */
 
-   test(`Add Product- ${products[0].name}`, async ({ page }) => {
-    await loginAsStandardUser(page)
-
-    const productPage = new ProductsPage(page)
-
-    await productPage.addProductToCart(products[0].name);
-    
-    await productPage.verifyCartCount(products[0].expectedCartCount);
+  test("Add one Product to cart @regression ", async ({ page }) => {
+    await productPage.addSingleProduct();
   });
 
   test("TC_007 - Remove product from cart @regression", async ({ page }) => {
-    await loginAsStandardUser(page);
-    const productsPage = new ProductsPage(page);
-    await productsPage.addProductToCart(products[0].name);
-    await productsPage.verifyCartCount(1);
-    await productsPage.removeProductFromCart(products[0].name);
-    await productsPage.verifyCartCount(0);
+    await productPage.addSingleProduct();
+    await productPage.removeSingleProduct();
   });
 
   test("TC_008 - Add multiple products to cart @cart @regression", async ({
     page,
   }) => {
-    await loginAsStandardUser(page);
-    const productsPage = new ProductsPage(page);
-
-    for (const product of products) {
-      await productsPage.addProductToCart(product.name);
-    }
-
-    await productsPage.verifyCartCount(products.length);
+    await productPage.addMultipleProduct();
   });
 
   test("TC_009 - Cart page should show selected products @cart @regression", async ({
     page,
   }) => {
-    await loginAsStandardUser(page);
-    const productsPage = new ProductsPage(page);
-
-    for (const product of products) {
-      await productsPage.addProductToCart(product.name);
-    }
-
-    await productsPage.goToCart();
-    const cartPage = new CartPage(page);
-
-    for (const product of products) {
-      await cartPage.verifyProductInCart(product.name);
-    }
+    await productPage.addMultipleProduct();
+    await productPage.goToCart();
+    await cartPage.verifyMultipleproductadded();
   });
 });
