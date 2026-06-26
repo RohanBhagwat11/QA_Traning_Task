e05ad9d4e0ac1fc75c3644924edb1a7454471d06
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-26 12:42:28 +0530

    Update CheckoutPage.ts

--- Day 3/pages/CheckoutPage.ts
+++ Day 3/pages/CheckoutPage.ts
@@ -1,6 +1,7 @@
-import { Page, Locator, expect } from '@playwright/test';
-import {routes} from '../Constants/routes'
-import {errorMsg} from '../Constants/errorMsg'
+import { Page, Locator, expect } from "@playwright/test";
+import { routes } from "../Constants/routes";
+import { errorMsg } from "../Constants/errorMsg";
+import { userdata } from "../test-data/userdata";
 
 export class CheckoutPage {
   readonly page: Page;
@@ -23,10 +24,46 @@ export class CheckoutPage {
     this.confirmationHeader = page.locator('[data-test="complete-header"]');
   }
 
-  async fillCheckoutDetails(firstName: string, lastName: string, postalCode: string): Promise<void> {
+  async fillCheckoutDetails(
+    firstName: string,
+    lastName: string,
+    postalCode: string,
+  ): Promise<void> {
     await this.firstNameInput.fill(firstName);
     await this.lastNameInput.fill(lastName);
     await this.postalCodeInput.fill(postalCode);
+  }
+
+  async fillValidCheckoutDetails(): Promise<void> {
+    await this.fillCheckoutDetails(
+      userdata[0].firstname,
+      userdata[0].lastname,
+      userdata[0].postalcode,
+    );
+  }
+
+  async fillWithMissingFirstName(): Promise<void> {
+    await this.fillCheckoutDetails(
+      "",
+      userdata[0].lastname,
+      userdata[0].postalcode,
+    );
+  }
+
+  async fillWithMissingLastName(): Promise<void> {
+    await this.fillCheckoutDetails(
+      userdata[0].firstname,
+      "",
+      userdata[0].postalcode,
+    );
+  }
+
+  async fillWithMissingPostalcode(): Promise<void> {
+    await this.fillCheckoutDetails(
+      userdata[1].firstname,
+      userdata[1].lastname,
+      "",
+    );
   }
 
   async continueCheckout(): Promise<void> {
@@ -35,6 +72,18 @@ export class CheckoutPage {
 
   async verifyValidationMessage(expectedMessage: string): Promise<void> {
     await expect(this.errorMessage).toContainText(expectedMessage);
+  }
+
+  async verifyMissingFirstNameMessage(): Promise<void> {
+    await this.verifyValidationMessage(errorMsg.firstNameRequired);
+  }
+
+  async verifyMissingLastNameMessage(): Promise<void> {
+    await this.verifyValidationMessage(errorMsg.lastNameRequired);
+  }
+
+  async verifyMissingPostalCodeMessage(): Promise<void> {
+    await this.verifyValidationMessage(errorMsg.postalCodeRequired);
   }
 
   async finishOrder(): Promise<void> {
