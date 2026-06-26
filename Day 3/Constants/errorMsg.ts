d6aa3ca41dbdd62908be1272384b2cefc0435e54
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-26 12:50:45 +0530

    Update errorMsg.ts

--- Day 3/Constants/errorMsg.ts
+++ Day 3/Constants/errorMsg.ts
@@ -1,8 +1,9 @@
 export const errorMsg = {
-    firstNameRequired : 'First Name is required',
-    lastNameRequired  : 'Last Name is required',
-    postalCodeRequired : 'Postal Code is required',
-    NoUserPassMatch : 'Username and password do not match',
-    lockedUserMsg : 'Sorry, this user has been locked out',
-    OrderSuccess: 'Thank you for your order'
-}
+  firstNameRequired: "First Name is required",
+  lastNameRequired: "Last Name is required",
+  postalCodeRequired: "Postal Code is required",
+  NoUserPassMatch: "Username and password do not match",
+  lockedUserMsg: "Sorry, this user has been locked out",
+  OrderSuccess: "Thank you for your order",
+  invalidUserMsg: "Username and password do not match any user in this service",
+};
