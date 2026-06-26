5ed203f30b837d50125c1802d15ca0c7f686333d
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-26 12:54:23 +0530

    Update Sidelink.spec.ts

--- Day 3/tests/Sidelink.spec.ts
+++ Day 3/tests/Sidelink.spec.ts
@@ -2,13 +2,16 @@ import { test, expect, Page } from "@playwright/test";
 import { loginAsStandardUser } from "../utils/testHelpers";
 import { SidelinkPage } from "../pages/SidelinkPage";
 
+let sidelinkPage: SidelinkPage;
+
 test.describe("Logout Tests", () => {
-  test("TC_014 - User should be able to logout", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
+    sidelinkPage = new SidelinkPage(page);
     await loginAsStandardUser(page);
+  });
 
-    const logoutPage = new SidelinkPage(page);
-
-    await logoutPage.logout();
-    await logoutPage.verifyLogout();
+  test("TC_014 - User should be able to logout", async ({ page }) => {
+    await sidelinkPage.logout();
+    await sidelinkPage.verifyLogout();
   });
 });
